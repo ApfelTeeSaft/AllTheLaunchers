c51724bffcbb1c75320b169d5671b1b32890d2b2
ApfelTeeSaft <apfelteesaft@gmail.com>
2022-04-19 13:26:58 +0200

    Release

---
+++ Launcher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace AllTheLaunchers
+{
+    internal class Launcher
+    {
+        public static void LaunchRift()
+        {
+            Process.Start("Rift.exe");
+        }
+
+        public static void LaunchEra()
+        {
+            Process.Start("FortniteLauncher.exe");
+        }
+
+        public static void GithubPage(string url)
+        {
+            Process.Start(url);
+        }
+
+    }
+}
